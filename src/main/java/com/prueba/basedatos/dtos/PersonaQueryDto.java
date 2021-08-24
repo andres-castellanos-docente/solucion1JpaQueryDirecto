26e2f5003a2caf2871c165e8af91b40ee0a9c57d
Andres Castellanos <andresrobanesky@gmail.com>
2021-08-24 11:38:33 -0500

    primera version

---
+++ src/main/java/com/prueba/basedatos/dtos/PersonaQueryDto.java
@@ -0,0 +1,18 @@
+package com.prueba.basedatos.dtos;
+
+
+public interface PersonaQueryDto {
+
+    Long getId();
+
+    String getPnombre();
+
+    String getPapellido();
+
+    Long getTelefono();
+
+    String getEmail();
+
+    String getNombreciudad();
+
+}
